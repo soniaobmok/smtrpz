8daf568d6f1f7f9d1c46445fdb73fcc9e3fd88ec
Sofiia Obmok <sofiaobmok@gmail.com>
2020-05-25 13:33:55 +0300

    added lab1

---
+++ lab1/eurekaclient/src/main/java/com/concert/eurekaclient/Movie/MovieController.java
@@ -0,0 +1,56 @@
+package com.concert.eurekaclient.Movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path="/movie")
+public class MovieController {
+
+    @Autowired
+    private MovieRepository repository;
+
+    @RequestMapping(path="", method = RequestMethod.POST)
+    public @ResponseBody String add (@RequestBody Movie movie) throws ParseException {
+        //movie.setPurchaseDate();
+        repository.save(movie);
+        return "Saved";
+    }
+
+    @RequestMapping(path="", method = RequestMethod.GET)
+    public @ResponseBody
+    Iterable<Movie> getAll() {
+        return repository.findAll();
+    }
+
+    @RequestMapping(path="/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Movie> getById(@PathVariable int id) {
+        return repository.findById(id);
+    }
+
+    @RequestMapping(path="/{id}", method = RequestMethod.PUT)
+    public @ResponseBody String update(@PathVariable Integer id,
+                                       @RequestBody Movie movie) throws ParseException {
+        repository.customUpdate(id, movie.getTitle(),
+                (new SimpleDateFormat("dd/MM/yyyy")).parse(movie.getDate()),
+                movie.getGenre(), movie.getCompany(),
+                movie.getRating(), movie.getDuration());
+        return "Updated";
+    }
+
+    @RequestMapping(path="/{id}", method = RequestMethod.DELETE)
+    public @ResponseBody void delete(@PathVariable Integer id) {
+        Movie instance  = repository.find(id);
+        repository.delete(instance);
+    }
+
+    @RequestMapping(path="/find/{title}", method = RequestMethod.GET)
+    public @ResponseBody Movie findByTitle(@PathVariable String title) {
+        return repository.findByTitle(title);
+    }
+}
---
+++ lab1/eurekaclient/src/main/java/com/concert/eurekaclient/Movie/MovieRepository.java
@@ -0,0 +1,24 @@
+package com.concert.eurekaclient.Movie;
+
+import java.util.Date;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+    @Query("SELECT u FROM Movie u WHERE u.title = ?1")
+    Movie findByTitle(String title);
+
+    @Query("SELECT u FROM Movie u WHERE u.id = ?1")
+    Movie find(Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Movie u SET u.title = :title, u.date = :date, u.company = :company, u.genre = :genre, u.duration = :duration, u.rating = :rating WHERE u.id = :id")
+    Integer customUpdate(@Param("id") Integer id, @Param("title") String title,
+                         @Param("date") Date date, @Param("genre") String genre,
+                         @Param("company") String company, @Param("duration") Integer duration,
+                         @Param("rating") Integer rating);
+}
